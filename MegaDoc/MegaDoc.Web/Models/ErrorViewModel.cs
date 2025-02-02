ac61acbffdccb57ea8783b2bb3e069d7fd7287e1
Ati <ativassileva@gmail.com>
2025-02-02 09:55:58 +0200

    Created MVC project

---
+++ MegaDoc/MegaDoc.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MegaDoc.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
